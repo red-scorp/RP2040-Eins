8a6c6c235b07a7324f89b5662bf9a3c195777608
Andriy Golovnya <Andriy.Golovnya@molabo.eu>
2023-11-09 15:04:16 +0100

    Added rotation_blink.ino arduino example.

---
+++ src/arduino/rgb_led/rotation_blink/rotation_blink.ino
@@ -0,0 +1,47 @@
+/* @file rotation_blink
+ * @brief Blink the RGB LED in rotation
+ *
+ * This program blinks the RGB LED in rotation.
+ * The red LED is turned on for 1 second, then the green LED is turned on for 1 second, and then the blue LED is turned on for 1 second.
+ * This process is repeated.
+ *
+ * @date 2021/01/23
+ * @version 1.0.0
+*/
+
+#include <Arduino.h>
+
+/* RGB LED pin numbers are defined in the rp2040 SDK as PIN_LED_R, PIN_LED_G and PIN_LED_B when you select RP2040-Eins as a target board */
+
+void setup() {
+  // Set the pin modes for the LEDs as outputs
+  pinMode(PIN_LED_R, OUTPUT);
+  pinMode(PIN_LED_G, OUTPUT);
+  pinMode(PIN_LED_B, OUTPUT);
+}
+
+void loop() {
+  // Turn on the red LED and turn off the others
+  digitalWrite(PIN_LED_R, HIGH);
+  digitalWrite(PIN_LED_G, LOW);
+  digitalWrite(PIN_LED_B, LOW);
+
+  // Wait for 1 second
+  delay(1000);
+
+  // Turn on the green LED and turn off the others
+  digitalWrite(PIN_LED_R, LOW);
+  digitalWrite(PIN_LED_G, HIGH);
+  digitalWrite(PIN_LED_B, LOW);
+
+  // Wait for 1 second
+  delay(1000);
+
+  // Turn on the blue LED and turn off the others
+  digitalWrite(PIN_LED_R, LOW);
+  digitalWrite(PIN_LED_G, LOW);
+  digitalWrite(PIN_LED_B, HIGH);
+
+  // Wait for 1 second
+  delay(1000);
+}
