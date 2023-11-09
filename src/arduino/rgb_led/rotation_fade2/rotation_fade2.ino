2026e7337e1e2840a8df38f2d97067981d085f55
Andriy Golovnya <Andriy.Golovnya@molabo.eu>
2023-11-09 15:18:38 +0100

    Added rotation_fade2.ino arduino example.

---
+++ src/arduino/rgb_led/rotation_fade2/rotation_fade2.ino
@@ -0,0 +1,42 @@
+/* @file rotation_fade2
+ * @brief Fade the RGB LED in rotation
+ *
+ * This program fades the RGB LED in rotation.
+ * The LED is fades from red to green, then from green to blue, and then from blue to red.
+ * This process is repeated.
+ *
+ * @date 2021/01/23
+ * @version 1.0.0
+ */
+
+#include <Arduino.h>
+
+/* RGB LED pin numbers are defined in the rp2040 SDK as PIN_LED_R, PIN_LED_G and PIN_LED_B when you select RP2040-Eins as a target board */
+
+const int fadeDuration = 1000; // 1 second fade duration
+
+void setup() {
+  pinMode(PIN_LED_R, OUTPUT);
+  pinMode(PIN_LED_G, OUTPUT);
+  pinMode(PIN_LED_B, OUTPUT);
+}
+
+void loop() {
+  fadeLED(PIN_LED_R, PIN_LED_G);
+  fadeLED(PIN_LED_G, PIN_LED_B);
+  fadeLED(PIN_LED_B, PIN_LED_R);
+}
+
+void fadeLED(int fromPin, int toPin) {
+  for (int brightness = 0; brightness <= 255; brightness++) {
+    analogWrite(fromPin, 255 - brightness);
+    analogWrite(toPin, brightness);
+    delay(fadeDuration / 255);
+  }
+
+  for (int brightness = 255; brightness >= 0; brightness--) {
+    analogWrite(fromPin, 255 - brightness);
+    analogWrite(toPin, brightness);
+    delay(fadeDuration / 255);
+  }
+}
